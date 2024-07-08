d6acbe4e20bce0e6c4ca9bfd64aef1031ff8306b
MAHAMMADARIF G A <92202566+arifg07@users.noreply.github.com>
2024-07-08 09:39:00 +0530

    Add files via upload

---
+++ food-ordering-app-frontend-main/food-ordering-app-frontend-main/src/AppRoutes.tsx
@@ -0,0 +1,47 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Layout from "./layouts/layout";
+import HomePage from "./pages/HomePage";
+import AuthCallbackPage from "./pages/AuthCallbackPage";
+import UserProfilePage from "./pages/UserProfilePage";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import ManageRestaurantPage from "./pages/ManageRestaurantPage";
+import SearchPage from "./pages/SearchPage";
+import DetailPage from "./pages/DetailPage";
+import OrderStatusPage from "./pages/OrderStatusPage";
+
+const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Layout showHero children={<HomePage />} />} />
+      <Route
+        path="/auth-callback"
+        element={<Layout children={<AuthCallbackPage />} />}
+      />
+      <Route
+        path="/search/:city"
+        element={<Layout children={<SearchPage />} />}
+      />
+      <Route
+        path="/detail/:restaurantId"
+        element={<Layout children={<DetailPage />} />}
+      />
+      <Route element={<ProtectedRoute />}>
+        <Route
+          path="/user-profile"
+          element={<Layout children={<UserProfilePage />} />}
+        />
+        <Route
+          path="/order-status"
+          element={<Layout children={<OrderStatusPage />} />}
+        />
+        <Route
+          path="/manage-restaurant"
+          element={<Layout children={<ManageRestaurantPage />} />}
+        />
+      </Route>
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
+
+export default AppRoutes;
